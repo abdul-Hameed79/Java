55d4d1e3c38fb6b679a6feb036438166c88d1e52
Hameed <sd.hameed79@gmail.com>
2024-03-28 22:54:06 +0530

    Next Topic added

--- Functions/SumOfInteger.java
+++ Functions/SumOfInteger.java
@@ -1,9 +1,12 @@
+// write a java program to print the sum of numbers in an interger
+
 package Functions;
 
 public class SumOfInteger {
+    // function to calculate the sum of intergers in a given interger
     public static void countNum(int n) {
         int revNum = 0;
-
+        // adding the numbers
         while(n != 0) {
             int rem = n % 10;
             revNum = revNum + rem;
@@ -13,6 +16,7 @@ public class SumOfInteger {
     }
 
     public static void main(String[] args) {
+        // function call
         countNum(3981);
     }
 }
