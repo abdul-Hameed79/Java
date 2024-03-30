857214dc56dc3672d4514aeeee6afb3ab593357a
Hameed <sd.hameed79@gmail.com>
2024-03-30 15:32:02 +0530

    Next Topic added

---
+++ Arrays/LinearSearch.java
@@ -0,0 +1,27 @@
+/* An array of numbers are given, in this array print the target value */
+
+package Arrays;
+
+public class LinearSearch {
+    static int foundTarget(int[] arr, int target) {
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i]==target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,4,5,7,8,1,9};
+        int target = 9;
+
+        int result = foundTarget(arr, target);
+
+        if(result == -1) {
+            System.out.println("Not found");
+        } else {
+            System.out.println("Target found at index: " + result);
+        }
+    } 
+}
