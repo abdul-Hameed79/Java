f65175059dcc86f63f5c639d77cc66b0062114d9
Hameed <sd.hameed79@gmail.com>
2024-04-09 23:39:26 +0530

    Next Topic added

--- Example.java
+++ Example.java
@@ -1,19 +1,18 @@
 public class Example {
-    public static int printcountO(int[] arr) {
+    public static int printcount(int[] arr) {
         int count = 1;
-        int i = arr[0];
+        int maxNum = arr[0];
 
-        for(i=1; i<arr.length; i++) {
-            if (arr[i] > i)
+        for(int i=1; i<arr.length; i++) {
+            if (arr[i] > maxNum)
             count++;
-            i = arr[i];
+            maxNum = arr[i];
         }
         return count;
     }
     public static void main(String[] args) {
         int[] arr = {7, 3, 8, 4, 9};
 
-        System.out.println("The final count is: " + printcountO(arr));
+        System.out.println("The final count is: " + printcount(arr));
     }
 }
-
