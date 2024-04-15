44b60f3d0240350297b0083c9fd8869c8207f8aa
Hameed <sd.hameed79@gmail.com>
2024-04-15 23:58:02 +0530

    Next Topic added

---
+++ Arrays/KadanesAlgorithm.java
@@ -0,0 +1,26 @@
+// write a java program to print sum of maximum sub array by using kadane's algorithm
+// Time complexity is O(n); space complexity is O(1);
+
+package Arrays;
+
+public class KadanesAlgorithm {
+    // Function to find max sub array sum
+    public static void maxSubArraySum(int[] arr) {
+        int maxsum = arr[0];
+        int currentsum = arr[0];
+
+        for(int i=1; i<arr.length; i++) {
+            currentsum = Math.max(arr[i], currentsum + arr[i]);
+            maxsum = Math.max(currentsum, maxsum);
+        }
+        System.out.println(maxsum);
+    }
+
+    // Main.java
+    public static void main(String[] args) {
+        int[] arr = {2, -3, 4, 1, -9};
+
+        // Function call
+        maxSubArraySum(arr);
+    }
+}
