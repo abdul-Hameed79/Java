29fe47f15ffb2aa55c5a682847626126cc201b9f
Hameed <sd.hameed79@gmail.com>
2024-04-23 17:59:49 +0530

    Person details file added

---
+++ CollegeManagementSystem/Person.java
@@ -0,0 +1,13 @@
+package CollegeManagementSystem;
+
+class Person {
+    protected String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getDetails() {
+        return name;
+    }
+}
