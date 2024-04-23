f31ba8ef5fcbc1a090e5e0849632c448ab441bc2
Hameed <sd.hameed79@gmail.com>
2024-04-23 18:02:26 +0530

    Solving example problems using var keyword instead of data types

---
+++ Example_Problems/Example1.java
@@ -0,0 +1,12 @@
+package Example_Problems;
+
+class Example1 {
+    public static void main(String[] args) {
+        // Instead of writing int, string, char we can use 'var' keyword in java the compailer will understand what type of data it is
+        var age = 20;
+        var name = "Hameed";
+        var grade = 'A';
+
+        System.out.println("Name: " + name + "\nAge: " + age + "\nGrade: " + grade);
+    }
+}
